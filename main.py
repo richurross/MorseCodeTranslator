7f6a4edc362228a2f7dd577c9395abbbf85ad562
richurross <richh.t.le@gmail.com>
2023-01-09 23:07:00 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+morse_code = {
+    'A': '.-',
+    'B': '-...',
+    'C': '-.-.',
+    'D': '-..',
+    'E': '.',
+    'F': '..-.',
+    'G': '--.',
+    'H': '....',
+    'I': '..',
+    'J': '.---',
+    'K': '-.-',
+    'L': '.-..',
+    'M': '--',
+    'N': '-.',
+    'O': '---',
+    'P': '.--.',
+    'Q': '--.-',
+    'R': '.-.',
+    'S': '...',
+    'T': '-',
+    'U': '..-',
+    'V': '...-',
+    'W': '.--',
+    'X': '-..-',
+    'Y': '-.--',
+    'Z': '--..',
+    '0': '-----',
+    '1': '.----',
+    '2': '..---',
+    '3': '...--',
+    '4': '....-',
+    '5': '.....',
+    '6': '-....',
+    '7': '--...',
+    '8': '---..',
+    '9': '----.',
+    '.': '.-.-.-',
+    ',': '--..--',
+    '?': '..--..'
+}
+
+def convert_morse():
+    result = ""
+    x = input("Type in your sentence, shawty!\n").upper()
+    for letter in x:
+        if letter in morse_code:
+            result += morse_code[letter] + " "
+        elif letter == " ":
+            result += " / "
+        else:
+            pass
+    print(f"Your sentence translated into morse code is: \n{result}")
+
+convert_morse()
